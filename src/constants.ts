3cdf7cc0e15b09dd3aff2a88aadd41a19aa3f51d
Aditya Singh <adipratapsingh.aps@gmail.com>
2022-05-07 03:57:09 +0530

    OLD and NEW tax regimes without deduction added

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,91 @@
 export const PORT = 8080;
+export const OLD_TAX_REGIME = {
+    slabs: [
+        {
+            gt: 0,
+            lte: 250000,
+            rateMultiplier: 0,
+            flatFees: 0,
+        },
+        {
+            gt: 250000,
+            lte: 500000,
+            rateMultiplier: 0.05,
+            flatFees: 0,
+        },
+        {
+            gt: 500000,
+            lte: 750000,
+            rateMultiplier: 0.2,
+            flatFees: 12500,
+        },
+        {
+            gt: 750000,
+            lte: 1000000,
+            rateMultiplier: 0.2,
+            flatFees: 62500,
+        },
+        {
+            gt: 1000000,
+            lte: 1250000,
+            rateMultiplier: 0.3,
+            flatFees: 112500,
+        },
+        {
+            gt: 1250000,
+            lte: 1500000,
+            rateMultiplier: 0.3,
+            flatFees: 187500,
+        },
+        {
+            gt: 1500000,
+            rateMultiplier: 0.3,
+            flatFees: 262500,
+        },
+    ],
+};
+export const NEW_TAX_REGIME = {
+    slabs: [
+        {
+            gt: 0,
+            lte: 250000,
+            rateMultiplier: 0,
+            flatFees: 0,
+        },
+        {
+            gt: 250000,
+            lte: 500000,
+            rateMultiplier: 0.05,
+            flatFees: 0,
+        },
+        {
+            gt: 500000,
+            lte: 750000,
+            rateMultiplier: 0.1,
+            flatFees: 12500,
+        },
+        {
+            gt: 750000,
+            lte: 1000000,
+            rateMultiplier: 0.15,
+            flatFees: 37500,
+        },
+        {
+            gt: 1000000,
+            lte: 1250000,
+            rateMultiplier: 0.2,
+            flatFees: 75000,
+        },
+        {
+            gt: 1250000,
+            lte: 1500000,
+            rateMultiplier: 0.25,
+            flatFees: 125000,
+        },
+        {
+            gt: 1500000,
+            rateMultiplier: 0.3,
+            flatFees: 187500,
+        },
+    ],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,84 @@
 import express from "express";
-import { PORT } from "./constants";
+import { PORT, OLD_TAX_REGIME, NEW_TAX_REGIME } from "./constants";
 
 const app = express();
 
 app.get("/", (_, res) => {
     res.send("Taxy is running...");
+});
+
+app.get("/getTaxRates/:regime", (req, res) => {
+    const regime = req?.params?.regime;
+    if (regime) {
+        if (regime === "old") {
+            res.send(OLD_TAX_REGIME);
+        } else if (regime === "new") {
+            res.send(NEW_TAX_REGIME);
+        } else {
+            res.status(404).send(`No such regime: ${regime}`);
+        }
+    } else {
+        res.status(404).send("No regime provided");
+    }
+});
+
+app.get("/getTaxAmount/:regime/:amount", (req, res) => {
+    const regime = req?.params?.regime;
+    const amount = req?.params?.amount;
+    if(!regime) {
+        res.status(404).send("No regime provided");
+    } else if(!amount) {
+        res.status(404).send("No amount provided");
+    } else {
+        const taxRates = regime === "old" ? OLD_TAX_REGIME : NEW_TAX_REGIME;
+        const taxSlabs = taxRates?.slabs;
+        if (taxSlabs) {
+            let taxableAmount = parseInt(amount);
+            let taxPayable = 0.0;
+            let taxedAmount = 0.0;
+            let prevLte = 0.0;
+            const result: {slabs: Array<unknown>, totalPayableTax: number} = {
+                totalPayableTax: 0.0,
+                slabs: [],
+            };
+            for (const taxSlab of taxSlabs) {
+                if ("gt" in taxSlab && taxableAmount > taxSlab.gt) {
+                    taxSlab.flatFees = 0;
+                    if (taxSlab.lte && taxableAmount > taxSlab.lte) {
+                        const amt = taxSlab.lte - taxSlab.gt;
+                        const tax = (taxSlab.rateMultiplier * amt) + taxSlab.flatFees;
+                        taxPayable += tax;
+                        taxedAmount += amt;
+                        prevLte = taxSlab.lte;
+                        const res = {
+                            slab: taxSlab,
+                            amount: amt,
+                            tax,
+                            taxPayable,
+                            taxedAmount,
+                        };
+                        result.slabs.push(res);
+                        result.totalPayableTax = taxPayable;
+                    } else {
+                        const amt = taxableAmount-prevLte;
+                        const tax = (taxSlab.rateMultiplier * amt) + taxSlab.flatFees;
+                        taxPayable += tax;
+                        const res = {
+                            slab: taxSlab,
+                            amount: amt,
+                            tax,
+                            taxPayable,
+                            taxedAmount,
+                        }
+                        result.slabs.push(res);
+                        result.totalPayableTax = taxPayable;
+                    }
+                }
+            }
+
+            res.send(result);
+        }
+    }
 });
 
 app.listen(PORT, () => {
