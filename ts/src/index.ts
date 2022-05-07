56da6e0ab2b90251cf4af286aa42a1f700cb0b9b
Aditya Singh <adipratapsingh.aps@gmail.com>
2022-05-07 09:04:18 +0530

    Added listener for SIGINT

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,6 +1,17 @@
 import { PORT } from "./constants";
 import { app } from "./app";
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server started at ${PORT}...`);
+});
+
+process.on('SIGINT', () => {
+    console.log("SIGINT received!");
+    if (server) {
+        console.log("Stopping server...");
+        server.close();
+        console.log("Server stopped!");
+    }
+    console.log("Bye!");
+    process.exit(1);
 });
