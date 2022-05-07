08b9f102de20683c7e940046ceddec4abd14848d
Aditya Singh <adipratapsingh.aps@gmail.com>
2022-05-07 12:28:39 +0530

    Updated e2e test

--- ts/test/e2e/homeEndpoints.test.ts
+++ ts/test/e2e/homeEndpoints.test.ts
@@ -1,9 +1,18 @@
-import { appServer } from '../../src/serverSetup'; 
+import { server } from '../../src/server'; 
+import { getHomeRoutes } from '../../src/routes/home';
 import supertest from 'supertest';
 
 describe("Home page", () => {
-    test("should return 200", async () => {
-        const response = await supertest(appServer).get('/');
+    test("should return 404 as I did not add route", async () => {
+        const homeSupertest = supertest(server);
+        const response = await homeSupertest.get('/');
+        expect(response.status).toBe(404);
+    });
+
+    test("should return 200 as I added route", async () => {
+        const homeSupertest = supertest(server);
+        server.use("/", getHomeRoutes());
+        const response = await homeSupertest.get('/');
         expect(response.status).toBe(200);
         expect(response.text).toEqual("Taxy - to save the day!");
     });
